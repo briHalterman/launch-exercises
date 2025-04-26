b730ad43f6b4855f8a16bcd928915f6ae6305e4f
Brittany Halterman <halterma@gmail.com>
2025-04-26 15:23:37 -0400

    Complete more exercises

---
+++ true_and_false.rb
@@ -0,0 +1,16 @@
+# a = 5
+# a = 0
+# a = ""
+a = 'false'
+
+if a
+  puts "how can this be true?"
+else
+  puts "it is not true"
+end
+
+# if x = 5
+#   puts "how can this be true?"
+# else
+#   puts "it is not true"
+# end
